d8e131296092b8fd47a6786ed7feee2866389fee
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-17 18:59:45 +0530

    Add select and submit state to QuestionCard component

--- components/shared/QuestionCard.tsx
+++ components/shared/QuestionCard.tsx
@@ -1,6 +1,6 @@
+"use client";
 import Link from "next/link";
-import { title } from "process";
-import React from "react";
+import React, { useState } from "react";
 interface QuestionCardProps {
   id: string;
   question: string;
@@ -18,6 +18,9 @@ const optionMapping: OptionMapping = {
 };
 
 const QuestionCard = ({ id, question, options, title }: QuestionCardProps) => {
+  const [select, setSelect] = useState<string>();
+  const [submit, setSubmit] = useState<boolean>(false);
+
   return (
     <div className=" flex justify-between gap-36">
       <div className="w-1/2">
@@ -27,23 +30,42 @@ const QuestionCard = ({ id, question, options, title }: QuestionCardProps) => {
       <div className=" flex flex-col w-1/2 gap-6">
         {options.map((option, index) => (
           <div
+            onClick={() => {
+              setSelect(option);
+              console.log(select);
+            }}
             key={index}
-            className="flex gap-8 bg-pureWhite px-5 py-5 rounded-3xl dark:bg-navyCustom items-center  shadow-lg  dark:shadow-none shadow-neutral-200"
+            className={`${
+              select === option ? "border-purpleCustom" : "border-transparent"
+            } flex gap-8 bg-pureWhite px-5 py-5 rounded-3xl dark:bg-navyCustom items-center  shadow-lg cursor-pointer dark:shadow-none shadow-neutral-200 group border-[3px]`}
           >
             <div className=" w-14 h-14 bg-lightGray p-3 flex items-center justify-center rounded-xl ">
-              <h3 className=" text-heading-s dark:text-grayNavy">
+              <h3 className=" text-heading-s dark:text-grayNavy group-hover:text-purpleCustom">
                 {optionMapping[index]}
               </h3>
             </div>
             <h3 className=" text-heading-s">{option}</h3>
           </div>
         ))}
-        <Link
-          href={`/${title}/${parseInt(id) + 1}`}
-          className="flex bg-purpleCustom px-5 py-7 rounded-3xl  items-center  shadow-lg  dark:shadow-neutral-100/10 shadow-neutral-300  text-heading-s text-pureWhite justify-center"
-        >
-          Next Question
-        </Link>
+
+        {!submit ? (
+          <button
+            onClick={() => {
+              setSubmit(true);
+            }}
+            disabled={!select}
+            className="flex bg-purpleCustom px-5 py-7 rounded-3xl  items-center  shadow-lg  dark:shadow-neutral-100/10 shadow-neutral-300  text-heading-s text-pureWhite justify-center"
+          >
+            Submit
+          </button>
+        ) : (
+          <Link
+            href={`/${title}/${parseInt(id) + 1}`}
+            className="flex bg-purpleCustom px-5 py-7 rounded-3xl  items-center  shadow-lg  dark:shadow-neutral-100/10 shadow-neutral-300  text-heading-s text-pureWhite justify-center"
+          >
+            Next Question
+          </Link>
+        )}
       </div>
     </div>
   );
